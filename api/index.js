4935731595f0a7fbd8cd0aad0c888630fcf8889b
Silvia Hernandez Marquez <silvia03hm@gmail.com>
2025-04-02 16:30:04 -0600

    Coriige errores en index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routerApi = require('./routes/index');
+const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
@@ -9,38 +9,33 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Configurar CORS
-const whitelist = ['http://127.0.0.1:5500'];
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.co', 'http://localhost:3000'];
 const options = {
   origin: (origin, callback) => {
-     if (whitelist.includes(origin)) {
-       callback(null, true);
-     } else {
-       callback(new Error('No permitido por CORS'));
-     }
-   }
- };
-
-
-
-// Middleware para JSON
-//app.use(cors());
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
 app.use(cors(options));
 
-// Rutas
+app.get('/api', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/api/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
 routerApi(app);
 
-// Rutas de ejemplo
-app.get('/api', (req, res) => res.send('Hola, servidor en Express'));
-app.get('/api/nueva-ruta', (req, res) => res.send('Soy una nueva ruta'));
-app.get('/api/home', (req, res) => res.send('Bienvenido a la home'));
-
-// Manejo de errores
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-// Iniciar servidor
+
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log('Mi port' +  port);
 });
